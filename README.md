8ef437e274f836aa55e33003dca1bbaeb83811b0
Paul Miller <paul@paulmillr.com>
2013-04-11 16:14:20 +0300

    Make stuff work.

---
+++ README.md
@@ -0,0 +1,42 @@
+# pushserve
+
+Dead-simple node.js pushState-enabled command-line http server.
+
+## Usage
+
+* Install it with `npm`: `npm install -g pushserve`.
+* Launch: `pushserve`.
+* You may specify port (default is 8000): `pushserve --port 4567`.
+
+```
+  Usage: pushserve [options]
+
+  Options:
+
+    -h, --help              output usage information
+    -V, --version           output the version number
+    -p, --port <port>       Web server port [8000]
+    -P, --path <path>       Path [.]
+    -i, --indexPath <path>  Path to file which to which 404s will be redirected [index.html]
+    -c, --noCors            Disable cross-origin resource sharing
+    -s, --noPushstate       Disable pushState
+```
+
+Node.js API:
+
+```javascript
+var pushserve = require('pushserve');
+// Any of these ways.
+pushserve();
+pushserve(function(error, options) {
+  console.log('Launched');
+});
+pushserve({port: 4567, indexPath: '../index.html', noCors: true});
+pushserve({port: 5555}, function() {
+  console.log('Launched');
+});
+```
+
+## License
+
+MIT
--- index.js
+++ index.js
@@ -2,22 +2,25 @@ var express = require('express');
 var http = require('http');
 var sysPath = require('path');
 
-// startServer :: String -> Number -> {base: String, indexPath: String, cors: Boolean, pushState: Boolean} -> Function
-var startServer = function(path, port, options, callback) {
+var startServer = function(options, callback) {
   // Specify default options.
-  if (path == null) path = '.';
-  if (port == null) port = 8000;
+  if (typeof options === 'function') {
+    callback = options;
+    options = null;
+  }
   if (options == null) options = {};
+  if (options.path == null) options.path = '.';
+  if (options.port == null) options.port = 8000;
   if (options.base == null) options.base = '';
-  if (options.indexPath == null) options.indexPath = sysPath.join(path, 'index.html')
-  if (options.cors == null) options.cors = true;
-  if (options.pushState == null) options.pushState = true;
+  if (options.indexPath == null) options.indexPath = sysPath.join(options.path, 'index.html')
+  if (options.noCors == null) options.noCors = false;
+  if (options.noPushState == null) options.noPushState = false;
   if (callback == null) callback = Function.prototype;
 
   var app = express();
 
   // Send cross-origin resource sharing enabling header.
-  if (options.cors) {
+  if (!options.noCors) {
     app.use(function(request, response, next) {
       response.header('Cache-Control', 'no-cache');
       response.header('Access-Control-Allow-Origin', '*');
@@ -26,20 +29,20 @@ var startServer = function(path, port, options, callback) {
   }
 
   // Route all static files to http paths.
-  app.use(options.base, express.static(path));
+  app.use(options.base, express.static(options.path));
 
   // Route all non-existent files to `index.html`
-  if (options.pushState) {
+  if (!options.noPushState) {
     app.all('' + options.base + '/*', function(request, response) {
-      response.sendfile(indexPath);
+      response.sendfile(options.indexPath);
     });
   }
 
   // Wrap express app with node.js server in order to have stuff like server.stop() etc.
   var server = http.createServer(app);
-  server.listen(port, function(error) {
-    console.log('Serving HTTP on 0.0.0.0 port', port);
-    callback(error);
+  server.listen(options.port, function(error) {
+    console.log('Serving HTTP on 0.0.0.0 port', options.port);
+    callback(error, options);
   });
   return server;
 };
